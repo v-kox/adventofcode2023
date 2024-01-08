d1977c7baab1386833b8b3c1086b366fc4bd4a23
Vincent Kox <vincent.kox@tpv-tech.com>
2024-01-08 15:11:17 +0100

    performance improved day 6 code.
    Use quadratic formula i.s.o. brute force
    formatting fixes

--- utils.py
+++ utils.py
@@ -1,8 +1,19 @@
-""" collection of utility functions that seem to be helpful in multiple tasks """
+"""
+collection of utility functions that seem to be helpful in multiple tasks
+"""
 
 import re
+
 
 def get_integers_from_line(line: str) -> list[int]:
     """ Returns a list of integers present in the provided string """
     int_re = r"\d+"
     return [int(x) for x in re.findall(int_re, line)]
+
+
+def get_numbers_from_line(line: str) -> list[str]:
+    """
+    Returns a list of numbers (as string) present in the provided string
+    """
+    int_re = r"\d+"
+    return [x for x in re.findall(int_re, line)]
