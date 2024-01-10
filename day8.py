b02a32ccde0e5e814e7cb33076dc5cbf51a90874
Vincent Kox <vincent.kox@tpv-tech.com>
2024-01-10 12:04:21 +0100

    day 8 part 2

--- day8.py
+++ day8.py
@@ -1,7 +1,8 @@
 """ Advent of Code 2023 day 8 """
 
-from dataclasses import dataclass
+import math
 import re
+from dataclasses import dataclass
 
 INPUT1 = """\
 RL
@@ -25,6 +26,20 @@ ZZZ = (ZZZ, ZZZ)
 """
 EXPECTED2 = 6
 
+INPUT3 = """\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+EXPECTED3 = 6
+
 
 def test_case1():
     """ Test case for example 1 part 1 """
@@ -36,6 +51,11 @@ def test_case2():
     assert compute(INPUT2) == EXPECTED2
 
 
+def test_case3():
+    """ Test case for example part 2"""
+    assert compute2(INPUT3) == EXPECTED3
+
+
 @dataclass
 class Node:
     """ Representation of a network node"""
@@ -44,10 +64,25 @@ class Node:
     r: str
 
     @property
-    def is_end(self):
+    def is_end(self) -> bool:
+        """ Check if current node is end node"""
         return self.name == "ZZZ"
 
+    @property
+    def is_start_ghost(self) -> bool:
+        """ Check if current node is a ghost start node """
+        return self.name[-1] == "A"
+
+    @property
+    def is_end_ghost(self) -> bool:
+        """ Check if current node is a ghost end node """
+        return self.name[-1] == "Z"
+
     def get_next_nodes(self, direction: str):
+        """
+        Return the name of the next node based on
+        on the given direction
+        """
         match direction.upper():
             case "L":
                 result = self.l
@@ -63,7 +98,7 @@ class Node:
 
 def read_node_from_line(line: str) -> Node:
     """ Create node from input line """
-    re_node = r"[A-Z]{3}"
+    re_node = r"\w{3}"
 
     match = re.findall(re_node, line)
 
@@ -74,6 +109,7 @@ def read_node_from_line(line: str) -> Node:
 
 
 def count_steps_to_end(nodes: dict[str, Node], steps: str) -> int:
+    """ Count steps needed to go to end node """
     node = nodes["AAA"]
     n_steps = 0
 
@@ -85,8 +121,37 @@ def count_steps_to_end(nodes: dict[str, Node], steps: str) -> int:
     return n_steps
 
 
-def compute(data: str) -> int:
-    """ Compute the result for part 1 """
+def _count_step_1node_ghost(node: Node, nodes: dict[str, Node], steps) -> int:
+    """ Count how many steps are required to reach a (ghost) end node """
+    n_steps = 0
+
+    while not node.is_end_ghost:
+        next_node = node.get_next_nodes(steps[n_steps % len(steps)])
+        node = nodes[next_node]
+        n_steps += 1
+
+    return n_steps
+
+
+def count_steps_ghost(nodes: dict[str, Node], steps):
+    """
+    Returns the number of steps required to reach an end node
+    simultaneously for all starting nodes.
+
+    This happens by determining for each start node how many steps it
+    takes to reach an end node and then calculating the least common
+    multiplier (lcm). (paths loop in input.)
+    """
+    start_nodes = [node for node in nodes.values() if node.is_start_ghost]
+    n_steps_to_end = [
+        _count_step_1node_ghost(node, nodes, steps)
+        for node in start_nodes
+        ]
+    return math.lcm(*n_steps_to_end)
+
+
+def read_input(data: str) -> tuple[dict[str, Node], str]:
+    """ Read steps and nodes from input data"""
     re_steps = r"^[LR]+$"
     nodes: dict[str, Node] = {}
 
@@ -99,7 +164,21 @@ def compute(data: str) -> int:
             node = read_node_from_line(line)
             nodes[node.name] = node
 
+    return nodes, steps
+
+
+def compute(data: str) -> int:
+    """ Compute the result for part 1 """
+    nodes, steps = read_input(data)
+
     return count_steps_to_end(nodes, steps)
+
+
+def compute2(data: str) -> int:
+    """ Compute the result for part 2 """
+    nodes, steps = read_input(data)
+
+    return count_steps_ghost(nodes, steps)
 
 
 def main():
@@ -108,8 +187,10 @@ def main():
         data = f.read()
 
     result = compute(data)
+    result2 = compute2(data)
 
     print(f"{result=}")
+    print(f"{result2=}")
 
 
 if __name__ == '__main__':
