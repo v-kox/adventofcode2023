c474ebd3c4fd3c705019f9ffa0815c1a5ca44bda
Vincent Kox <vincent.kox@tpv-tech.com>
2024-01-14 18:39:08 +0100

    day10 part 2

--- day10.py
+++ day10.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+import functools
 from dataclasses import dataclass
 from enum import Enum
 from typing import Generator
+
+import pytest
 
 INPUT1 = """\
 .....
@@ -22,15 +25,72 @@ LJ...
 """
 EXPECTED2 = 8
 
+INPUT3 = """\
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"""
+EXPECTED3 = 4
 
-def test_case1():
+INPUT4 = """\
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+EXPECTED4 = 8
+
+INPUT5 = """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+
+EXPECTED5 = 10
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (INPUT1, EXPECTED1),
+        (INPUT2, EXPECTED2),
+    ],
+)
+def test_case_part1(input, expected):
     """Test case example 1 part 1"""
-    assert compute(INPUT1) == EXPECTED1
+    assert compute(input) == expected
 
 
-def test_case2():
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (INPUT3, EXPECTED3),
+        (INPUT4, EXPECTED4),
+        (INPUT5, EXPECTED5),
+    ],
+)
+def test_case_part2(input, expected):
     """Test case example 2 part 1"""
-    assert compute(INPUT2) == EXPECTED2
+    assert compute2(input) == expected
 
 
 class Offsets(Enum):
@@ -50,9 +110,9 @@ class Offsets(Enum):
 class Node:
     """Representation of a node in the grid"""
 
-    symbol: str
     row: int
     col: int
+    symbol: str = "."
 
     @property
     def is_start(self) -> bool:
@@ -118,9 +178,93 @@ class Grid:
                 continue
 
 
-def get_maze(grid: Grid) -> list[Node]:
+@dataclass
+class Maze:
+    nodes: list[Node]
+
+    @staticmethod
+    def _min_node(this: Node, that: Node) -> Node:
+        """Return a node object that has the minimum coords of both nodes"""
+        return Node(min(this.row, that.row), min(this.col, that.col))
+
+    @staticmethod
+    def _max_node(this: Node, that: Node) -> Node:
+        """Return a node object that has the maximum coords of both nodes"""
+        return Node(max(this.row, that.row), max(this.col, that.col))
+
+    @functools.cached_property
+    def bounding_box(self) -> tuple[int, int, int, int]:
+        """Determine the bounding box of maze polygon"""
+
+        min_node = functools.reduce(self._min_node, self.nodes)
+        max_node = functools.reduce(self._max_node, self.nodes)
+        return (min_node.row, max_node.row, min_node.col, max_node.col)
+
+    @functools.cached_property
+    def vertices(self) -> list[Node]:
+        """
+        Get the vertices of the maze.
+
+        Remove the nodes where the direction doesn't change.
+        i.e. vertical and horizontal nodes
+        """
+        return [n for n in self.nodes if n.symbol not in "|-"]
+
+    def _find_convex_hull_node(self) -> Node:
+        """
+        Find a Node on the convex hull of the polygon (outside).
+
+        Find the Node on the polygon that has the minimum column.
+        If multiple nodes, select the ones with the smalles row.
+        """
+        _, _, col_min, _ = self.bounding_box
+
+        # Find all nodes that lie in the min X position
+        nodes = [node for node in self.nodes if node.col == col_min]
+
+        if len(nodes) > 1:
+            # If multiple nodes, find one with lowest row value
+            row_min = min(node.row for node in nodes)
+            nodes = [node for node in nodes if node.row == row_min]
+
+        return nodes[0]
+
+    @property
+    def positive_orientation(self) -> bool:
+        """
+        Check that the node order of the maze is a positive orientation.
+        Use logic defined here:
+        https://en.wikipedia.org/wiki/Curve_orientation#Orientation_of_a_simple_polygon
+        """
+        node = self._find_convex_hull_node()
+
+        idx = self.vertices.index(node)
+        n_a = self.vertices[(idx - 1) % len(self.vertices)]
+        n_b = self.vertices[idx]
+        n_c = self.vertices[(idx + 1) % len(self.vertices)]
+
+        return (
+            (n_b.col * n_c.row + n_a.col * n_b.row + n_a.row * n_c.col)
+            - (n_a.row * n_b.col + n_b.row * n_c.col + n_a.col * n_c.row)
+        ) > 0
+
+
+@dataclass
+class Vertex:
+    n1: Node
+    n2: Node
+
+    @property
+    def direction(self):
+        if self.n1.col == self.n2.col:
+            return "V"
+        else:
+            return "H"
+
+
+def get_maze(grid: Grid) -> Maze:
     """Construct the maze for a given grid."""
-    maze = []
+    nodes = []
 
     current_node = grid.get_start_of_maze()
     # previous node is needed for pathing.
@@ -130,7 +274,7 @@ def get_maze(grid: Grid) -> list[Node]:
     end_of_maze = False
 
     while not end_of_maze:
-        maze.append(current_node)
+        nodes.append(current_node)
 
         neighbours = [
             n
@@ -149,20 +293,66 @@ def get_maze(grid: Grid) -> list[Node]:
             prev_node = current_node
             current_node = neighbours[0]
 
-    return maze
+    maze = Maze(nodes)
+
+    # Check maze orientation
+    if not maze.positive_orientation:
+        # Ensure maze in positive orientation
+        nodes.reverse()
+        maze = Maze(nodes)
+
+    return Maze(nodes)
+
+
+def compute_area_in_maze(maze: Maze) -> int:
+    """
+    Compute the are enclosed by the maze polygon using shoelace formula:
+    https://en.wikipedia.org/wiki/Shoelace_formula
+    """
+    area = 0
+    for idx in range(len(maze.vertices)):
+        n1 = maze.vertices[idx]
+        n2 = maze.vertices[(idx + 1) % len(maze.vertices)]
+        area += (n1.row + n2.row) * (n1.col - n2.col)
+
+    return area // 2
+
+
+def pick_theorem(maze: Maze, area: int) -> int:
+    """
+    Using the pick theorem too calculate how many nodes are included inside the
+    maze polygon:
+    https://en.wikipedia.org/wiki/Pick%27s_theorem
+    """
+    return area + 1 - len(maze.nodes) // 2
 
 
 def compute(data: str) -> int:
     """Compute the result for part 1"""
     g = [
-        [Node(c, rown, coln) for (coln, c) in enumerate(line)]
+        [Node(rown, coln, c) for (coln, c) in enumerate(line)]
         for (rown, line) in enumerate(data.splitlines())
     ]
 
     grid = Grid(g)
     maze = get_maze(grid)
 
-    return len(maze) // 2
+    return len(maze.nodes) // 2
+
+
+def compute2(data: str) -> int:
+    """Compute the result for part 2"""
+    g = [
+        [Node(rown, coln, c) for (coln, c) in enumerate(line)]
+        for (rown, line) in enumerate(data.splitlines())
+    ]
+
+    grid = Grid(g)
+    maze = get_maze(grid)
+
+    area = compute_area_in_maze(maze)
+
+    return pick_theorem(maze, area)
 
 
 def main() -> None:
@@ -171,8 +361,9 @@ def main() -> None:
         data = f.read()
 
     result = compute(data)
-
-    print(f"{result}")
+    result2 = compute2(data)
+    print(f"{result=}")
+    print(f"{result2=}")
 
 
 if __name__ == "__main__":
